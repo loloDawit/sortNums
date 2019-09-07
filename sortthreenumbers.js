7eae96ff02bd1937b79ab427b6092b22b81304cf
Dawit Abera <dawitabera64@gmail.com>
2019-09-07 10:42:07 -0700

    first commit

---
+++ sortthreenumbers.js
@@ -0,0 +1,29 @@
+var x,y,z; 
+var input = prompt(); 
+x = input; 
+var input2 = prompt();
+y = input2;
+var input3 = prompt(); 
+z = input3;
+
+if(x > y && x > z){
+    if(y>z){
+        console.log(x + "," + y + "," + z);
+    }else{
+        // y is less than z
+        console.log(x + "," + z + "," + y);
+    }
+}else if(y > x && y > z){
+    if(x > z){
+        console.log(y + "," + x + "," + z);
+    }else{
+        console.log(x + "," + z + "," + x);
+    }
+}else if(z > x && z > y){
+    if(x>y){
+        console.log(z + "," + x + "," + y);
+    }else{
+        console.log(z + "," + y + "," + x);
+    }
+}
+
